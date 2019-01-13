561ff190cb88dffe03d625a9404a7eca3481a200
Aparimeet <aparimeet@gmail.com>
2019-01-13 16:20:27 +0530

    Added Signup and Login

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def index
+  	@posts = Post.all
+  end
+
+  def show
+  	@post = Post.find(params[:id])
+  end
+
+  def create
+  	@post = Post.new(post_params)
+
+  	if(@post.save)
+      flash[:success] = "Success!"
+  		redirect_to @post
+  	else
+      flash[:error] = @post.error.full_messages
+  		render 'new'
+  	end
+  end
+
+  private 
+
+    def post_params
+  	 params.require(:post).permit(:title, :body, :image)
+    end
+
+end
---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+	has_attached_file :avatar, styles: { medium: "500x500>", thumb: "100x100>" }, default_url: "/images/:style/missing.png" 
+	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+end
